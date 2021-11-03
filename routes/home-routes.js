c142b0d317c61a3496e45a6a782e27db18d25a7d
Karen V <karen.pretzel.365@gmail.com>
2021-11-03 14:46:14 -0600

    added homeroutes for html display

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -3,5 +3,11 @@ const path = require("path");
 //const { User, Post, Comments } = require("../models");
 
 router.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, 'stats.html'));
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
   });
+
+
+  router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+  });
+  module.exports = router;
