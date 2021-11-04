76f0e9db0511982f240a785bbdde67c990440dc5
Karen V <karen.pretzel.365@gmail.com>
2021-11-03 18:19:13 -0600

    removed commented code

---
+++ routes/api/workout-routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const Workout  = require("../../models/Workout");
+
+//View the combined weight of multiple exercises from the past
+// seven workouts on the stats page.
+router.get("/weight", (req, res) => {
+    Workout.find()
+      .then(result => {
+        res.json(result);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+//View the total duration of each workout from the past 
+//seven workouts on the stats page.
+
+module.exports = router;
