3a80f939e53a9e6754bc05be0025fe74c97843d7
Karen V <karen.pretzel.365@gmail.com>
2021-11-12 19:57:25 -0600

    redownloaded node_modules

--- seeders/seed.js
+++ seeders/seed.js
@@ -126,7 +126,13 @@ const workoutSeed = [
   },
 ];
 
-
-  db.once('open', async () => {
-    await Workout.deleteMany({});
-   })
+db.Workout.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(workoutSeed))
+  .then((data) => {
+    console.log(data.result.n + ' records inserted!');
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
