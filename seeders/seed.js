7fbe89806d313219564af08b29e1423251b4e3f2
Karen V <karen.pretzel.365@gmail.com>
2021-11-12 19:29:54 -0600

    added the connnect

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-/*
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
-*/
+
 const workoutSeed = [
   {
     day: new Date(new Date().setDate(new Date().getDate() - 9)),
